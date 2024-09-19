36278a44a865934759854d82b78644f58c37d063
Sebastian Sepulveda Patiño <sebas20112322@gmail.com>
2024-09-18 23:09:49 -0500

    Creación del micro carrito

---
+++ src/main/java/com/microservice/microserviceshoppingcart/MicroServicioCarritoApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.microserviceshoppingcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServicioCarritoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServicioCarritoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microservice/microserviceshoppingcart/MicroServicioCarritoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.microserviceshoppingcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServicioCarritoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
